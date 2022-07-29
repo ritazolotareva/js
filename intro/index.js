25a9a079eada6260985d28514cd1a0ee5fcb00e6
Margarita Zolotareva <ritazolotareva@icloud.com>
2022-07-29 13:34:49 +0300

    init

---
+++ intro/index.js
@@ -0,0 +1,11 @@
+console.log('привет, мир')
+
+
+// const today = (x) => {}
+
+// const diff = (now) => (x) => new Date(x);
+// const countDays = (ms) => Math.round(ms / 1000 / 60 / 60)
+
+// console.log(diff)
+
+// let x = 42;

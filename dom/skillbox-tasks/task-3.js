0f34b793608102ff9741336e02a4e3c2d0285877
Margarita Zolotareva <ritazolotareva@icloud.com>
2023-07-28 18:20:11 +0200

    fixed the list

--- dom/skillbox-tasks/task-3.js
+++ dom/skillbox-tasks/task-3.js
@@ -2,19 +2,26 @@ function createStudentsList(listArr) {
     const h1 = document.createElement('h1');
     h1.textContent = 'List of students';
     document.body.append(h1);
-    
-    const div = document.createElement('div');
-    document.body.append(div);
-    
-    const values = Object.values(listArr);
-    for (let value of values) {
-        const ul = document.createElement('ul');
-        div.append(ul);
-        
-        let li = document.createElement('li')
 
-        li.textContent = `Имя: ${value['name']}, возраст: ${value['age']}`;
-        ul.prepend(li);
+    const div = document.createElement('div');
+    document.body.append(div)
+
+    const ul = document.createElement('ul');
+    div.append(ul);
+
+    for (let value of listArr) {
+        let list = [document.createElement('li'),]
+        for (let i = 0; i < list.length; ++i) {
+            const h2 = document.createElement('h2');
+            h2.textContent = `Имя: ${value['name']}`;
+            list[i].append(h2);
+            
+            const span = document.createElement('span');
+            span.textContent = `Возраст: ${value['age']}`;
+            list[i].append(span);
+
+            ul.append(list[i]);
+        }
     }
 }
 

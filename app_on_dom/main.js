12dac68abd39e58d59009ac2a182297c9fc5a99f
Margarita Zolotareva <ritazolotareva@icloud.com>
2023-08-05 23:04:31 +0200

    todo app developed

--- app_on_dom/main.js
+++ app_on_dom/main.js
@@ -33,13 +33,60 @@
         return list;
     }
 
+    function createTodoItem(name) {
+        let item = document.createElement('li');
+        let buttonGroup = document.createElement('div')
+        let doneButton = document.createElement('button')
+        let deleteButton = document.createElement('button')
+
+        item.classList.add('list-group-item', 'd-flex', 'justify-content-between', 'align-center');
+        item.textContent = name;
+        buttonGroup.classList.add('btn-group', 'btn-group-sm');
+        doneButton.classList.add('btn', 'btn-success');
+        doneButton.textContent = 'Done';
+        deleteButton.classList.add('btn', 'btn-danger');
+        deleteButton.textContent = 'Delete';
+
+        buttonGroup.append(doneButton);
+        buttonGroup.append(deleteButton);
+        item.append(buttonGroup);
+
+        return {
+            item, doneButton, deleteButton
+        }
+    }
+
     let container = document.getElementById('todo-app');
 
     let todoAppTitle = createAppTitle('To Do List');
     let todoItemForm = createTodoItemForm();
     let todoList = createTodoList();
+    // let todoItems = [createTodoItem('clean the bedroom'), createTodoItem('buy bread')];
 
     container.append(todoAppTitle);
     container.append(todoItemForm.form);
     container.append(todoList);
+    // todoList.append(todoItems[0].item);
+    // todoList.append(todoItems[1].item);
+
+    todoItemForm.form.addEventListener('submit', function(e) {
+        e.preventDefault();
+        if(!todoItemForm.input.value) {
+            return;
+        }
+
+        let todoItem = createTodoItem(todoItemForm.input.value);
+        todoItem.doneButton.addEventListener('click', function() {
+            todoItem.item.classList.toggle('list-group-item-success');
+        });
+        todoItem.deleteButton.addEventListener('click', function() {
+            if(confirm('Are you sure?')) {
+                todoItem.item.remove();
+            }
+        })
+
+        todoList.append(todoItem.item);
+        todoItemForm.input.value = '';
+    }
+    );
 })();
